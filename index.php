10b9e7ccf3125aabe54cdbaf7222d93853bb4a76
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-08-17 09:10:18 -0300

    feat - alterações na index.php, criação da tabela de usuario.

--- index.php
+++ index.php
@@ -8,9 +8,35 @@ include "infra/conexao.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>CRUD - Restaurante</title>
+    <link rel="stylesheet" href="style/styles.css">
 </head>
 <body>
     
+<main>
+        <h2>Adicione um novo Usuário!</h2>
+        <form action="public/cadastrar.php" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome">
+            <br>
+            <label for="email">Email:</label>
+            <input type="text" name="email">
+            <br>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha">
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <div>
+            <h2>Usuários Cadastrados</h2>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Senha</th>
+                </tr>
+
+
 </body>
 </html>
