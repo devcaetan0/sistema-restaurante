78101bf7311ca97775cba7d91939d031ece88dba
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-08-19 08:10:34 -0300

    feat - alterações nas funcionalidades dos pratos

--- public/pratos/pratos-editar.php
+++ public/pratos/pratos-editar.php
@@ -20,18 +20,46 @@ $pratos =mysqli_fetch_assoc($resultado);
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratos/title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Pratos</h1>
+    </header>
+    <main>
+
 <div>
-            <h2>Adicione um novo Prato!</h2>
+            <h2>Editando o Prato!</h2>
             <form action="public/usuarios-cadastrar.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $pratos["id"]?>">
+
                 <label for="nome">Nome:</label>
-                <input type="text" name="nome">
+                <input type="text" name="nome" value="<?php echo $pratos["nome"]?>">
                 <br>
-                <label for="email">Email:</label>
-                <input type="text" name="email">
+                <label for="email">Descrição:</label>
+                <input type="text" name="descricao" value="<?php echo $pratos["descricao"]?>">>
                 <br>
-                <label for="senha">Senha:</label>
-                <input type="password" name="senha">
+                <label for="senha">Preço:</label>
+                <input type="password" name="preco" value="<?php echo $pratos["preco"]?>">>
                 <br>
+                <label for="categoria">Categoria:</label>
+                <input type="text" name="categoria" value="<?php echo $categoria["categoria"]?>">>
+                <br>
+
+                <label for="id_usuario">Cadastrado por:</label>
+                <select name="id_usuario">
+                    <option value="0">Selecione</option>
+                </select>
+                <br>
+
                 <button type="submit">Atualizar</button>
             </form>
         </div>
